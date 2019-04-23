f0f92e46331bb41ddb53d7e7a16e68b9399d720c
Leeland Clenista <leeland_clenista@yahoo.com>
2019-04-22 20:06:55 -0700

    added functional component

--- src/WaterView.js
+++ src/WaterView.js
@@ -1,7 +1,9 @@
 import React from 'react';
 
 const WaterView = (props) => {
-    return(
+    //reusable functional component that changes 
+    //depending on the parent stateful component
+    return (
     <div>
         <button onClick={props.toggle}>{props.hideShow} div!</button>
         <div style={props.style}>{props.waterForm}</div>
